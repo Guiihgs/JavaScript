608443f88e81a856bb6347c450750b34edd062df
Guiihgs <224700364+Guiihgs@users.noreply.github.com>
2026-07-05 12:22:38 -0300

    Create ex001.js

---
+++ Modulo03-Condicoes/Aula01-Condicao_Simples/ex001.js
@@ -0,0 +1,6 @@
+var velo = 10.5
+console.log(`A velocidade do seu carro é de ${velo}Km/h`)
+if (velo > 60) {
+    console.log(`Voce ultrapassou a velocidade permitida, MULTADO!!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!!`)
